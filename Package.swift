c316314f1950e2e36c3a2e5faf051307b58f559e
siddharth <siddharth.k@mapmyindia.com>
2026-04-02 16:29:08 +0530

    Release version 6.1.4
    - Fixed issue for mapplsPin

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapplsMap",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-map/MapplsMap.xcframework-6.1.3.zip",
-            checksum: "f93ad4b46e2e46cac2f8c397409802fb2b9dd513ea1f25915db0c138b2705142"
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-map/MapplsMap.xcframework-6.1.4.zip",
+            checksum: "239f826001581e141eb44fc6bc572b3a39a0c9562cb12ac6936da69ac6c8391b"
         )
     ]
 )
